f75abefcdaef9152a0979ca1c3f8c73a199280bb
edonD <edoonn.d@gmail.com>
2022-03-20 13:53:10 +0100

    update-Multitheme

--- Components/Multitheme.js
+++ Components/Multitheme.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { fadeInUp as fadeInFromLib } from "react-animations";
 import { keyframes } from "styled-components";
 import Image from "next/image";
+
 function Multitheme() {
   const [theme, setTheme] = useState("livesport");
   const [active, setActive] = useState(false);
@@ -30,34 +31,174 @@ function Multitheme() {
       <DescriptionContainer>
         <MultithemeMenu>
           <a onClick={() => acitivisation("livesport")}>
-            <span>LIVE SPORTS</span>
+            <span>SPORTE LIVE</span>
           </a>
 
           <a onClick={() => acitivisation("breakingnews")}>
-            <span>BREAKING NEWS</span>
+            <span>LAJME</span>
           </a>
 
           <a onClick={() => acitivisation("liveevents")}>
-            <span>LIVE EVENTS</span>
+            <span>EVENTE LIVE</span>
           </a>
         </MultithemeMenu>
         <HeaderDescription>
           <h1>
             {theme === "livesport"
-              ? "Live Sports"
+              ? "Sporte Live"
               : theme === "breakingnews"
-              ? "Breaking News"
-              : "Live Events"}
+              ? "Lajme"
+              : "Evente Live"}
           </h1>
         </HeaderDescription>
         <SecondHeaderDescription active={active}>
-          <h2>
-            {theme === "livesport"
-              ? "Catch your games at home or on the go. Stream live games from major college and pro leagues including the NCAA®, NBA, NHL, NFL, and more."
-              : theme === "breakingnews"
-              ? "Keep pace with what's going on locally and globally with trusted opinions from all the top news networks."
-              : "Spectacular, can't-miss moments like the Olympics, Grammys®, Oscars®, Emmys®, and more."}
-          </h2>
+          {theme === "livesport" ? (
+            <>
+              <h2>
+                Të gjitha ndeshjet sportive në smartphone, TV apo Website. Ndiq
+                top ligat e futbollit, basktebollit, apo ndeshjet e boksit the
+                te UFC.
+              </h2>
+              <SecondImageContainer>
+                <ImageWrapper>
+                  <Image
+                    src='/images/multitheme/sports/Serie_a_2021.png'
+                    width={33}
+                    height={38}
+                  />
+                </ImageWrapper>
+                <ImageWrapper>
+                  <Image
+                    src='/images/multitheme/sports/icons8-the-premier-league-48.png'
+                    width={48}
+                    height={48}
+                  />{" "}
+                </ImageWrapper>
+                <ImageWrapper>
+                  <Image
+                    src='/images/multitheme/sports/LaLiga_Santander.svg'
+                    width={48}
+                    height={48}
+                  />
+                </ImageWrapper>
+                <ImageWrapper>
+                  <Image
+                    src='/images/multitheme/sports/Bundesliga.svg'
+                    width={38}
+                    height={48}
+                  />
+                </ImageWrapper>
+                <ImageWrapper>
+                  <Image
+                    src='/images/multitheme/sports/NBA.svg'
+                    width={38}
+                    height={48}
+                  />
+                </ImageWrapper>
+                <ImageWrapper>
+                  <Image
+                    src='/images/multitheme/sports/UFC.webp'
+                    width={68}
+                    height={58}
+                  />
+                </ImageWrapper>
+              </SecondImageContainer>
+            </>
+          ) : theme === "breakingnews" ? (
+            <>
+              <h2>
+                Informohu i pari për ngjarjet lokale dhe botërore. Debatet më të
+                shikuara me analistët më të suksesshem të vendit
+              </h2>
+              <SecondImageContainer>
+                <NewsImageWrapper>
+                  <Image
+                    src='/logo/topchannel.png'
+                    width={40}
+                    height={45}
+                    objectFit='contain'
+                  />
+                </NewsImageWrapper>
+                <NewsImageWrapper>
+                  <Image
+                    src='/logo/klank.png'
+                    width={38}
+                    height={38}
+                    objectFit='contain'
+                  />
+                </NewsImageWrapper>
+                <NewsImageWrapper>
+                  <Image
+                    src='/logo/klan.png'
+                    width={45}
+                    height={45}
+                    objectFit='contain'
+                  />
+                </NewsImageWrapper>
+                <NewsImageWrapper>
+                  <Image
+                    src='/logo/t7.png'
+                    width={40}
+                    height={40}
+                    objectFit='contain'
+                  />
+                </NewsImageWrapper>
+                <NewsImageWrapper style={{ backgroundColor: "#d8001a" }}>
+                  <Image
+                    src='/logo/atv.jpg'
+                    width={35}
+                    height={35}
+                    objectFit='contain'
+                  />
+                </NewsImageWrapper>
+                <NewsImageWrapper>
+                  <Image
+                    src='/logo/rtv21.png'
+                    width={35}
+                    height={35}
+                    objectFit='contain'
+                  />
+                </NewsImageWrapper>
+                <NewsImageWrapper>
+                  <Image
+                    src='/logo/rtk.jpg'
+                    width={45}
+                    height={45}
+                    objectFit='contain'
+                  />
+                </NewsImageWrapper>
+                <NewsImageWrapper>
+                  <Image
+                    src='/logo/dukagjini.png'
+                    width={45}
+                    height={45}
+                    objectFit='contain'
+                  />
+                </NewsImageWrapper>
+                <NewsImageWrapper>
+                  <Image
+                    src='/logo/kanal10.jpg'
+                    width={43}
+                    height={45}
+                    objectFit='contain'
+                  />
+                </NewsImageWrapper>
+                <NewsImageWrapper>
+                  <Image
+                    src='/logo/Ktv.png'
+                    width={43}
+                    height={45}
+                    objectFit='contain'
+                  />
+                </NewsImageWrapper>
+              </SecondImageContainer>
+            </>
+          ) : (
+            <h2>
+              Eventet që nuk duhet humbur. Nga koncertet prej artistëve më të
+              njohur deri te spektatlet e filmit dhe modës.
+            </h2>
+          )}
         </SecondHeaderDescription>
       </DescriptionContainer>
     </Container>
@@ -206,6 +347,40 @@ const DescriptionContainer = styled.div`
     width: 90%;
   }
 `;
+const SecondImageContainer = styled.div`
+  height: fit-content;
+  width: fit-content;
+  display: grid;
+  grid-gap: 10px;
+  gap: 10px;
+  grid-template-columns: repeat(10, minmax(50px, 1fr));
+  @media screen and (max-width: 1400px) {
+    grid-gap: 0px;
+    gap: 0px;
+    grid-template-columns: repeat(7, minmax(40px, 1fr));
+  }
+  @media screen and (max-width: 1000px) {
+    grid-gap: 10px;
+    gap: 10px;
+    grid-template-columns: repeat(4, minmax(40px, 1fr));
+  }
+`;
+const ImageWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 5px;
+`;
+
+const NewsImageWrapper = styled.div`
+  width: 50px;
+  height: 50px;
+  margin: 5px;
+  border-radius: 25px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const HeaderDescription = styled.div`
   width: 500px;
@@ -265,6 +440,8 @@ const SecondHeaderDescription = styled.div`
   }
   h2 {
     margin: 0px;
+    font-weight: 400;
+    font-size: 20px;
     @media screen and (max-width: 700px) {
       font-size: 15px;
     }
