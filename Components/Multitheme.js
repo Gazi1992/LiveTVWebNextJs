74f8ac0ee2d0806f8cde949758769bf2270078c7
edonD <edoonn.d@gmail.com>
2022-04-06 01:21:00 +0200

    final-update1

--- Components/Multitheme.js
+++ Components/Multitheme.js
@@ -40,21 +40,8 @@ function Multitheme() {
       </Square>
 
       <DescriptionContainer>
-        {/* <MultithemeMenu>
-          <a onClick={() => acitivisation("livesport")}>
-            <span>SPORTE LIVE</span>
-          </a>
-
-          <a onClick={() => acitivisation("breakingnews")}>
-            <span>LAJME</span>
-          </a>
-
-          <a onClick={() => acitivisation("liveevents")}>
-            <span>EVENTE LIVE</span>
-          </a>
-        </MultithemeMenu> */}
         <HeaderDescription>
-          <h1>Sporte Live</h1>
+          <h1>Të gjitha sportet</h1>
         </HeaderDescription>
         <SecondHeaderDescription>
           <h2>
@@ -111,85 +98,6 @@ function Multitheme() {
   );
 }
 
-const MultithemeMenu = styled.div`
-  align-items: center;
-  display: flex;
-  flex-flow: row nowrap;
-  height: 100px;
-  justify-content: flex-start;
-  cursor: pointer;
-  width: 100%;
-
-  margin-right: auto;
-  margin-left: 50px;
-  z-index: 5;
-  a {
-    display: flex;
-    align-items: center;
-    padding: 0 12px;
-    @media (max-width: 500px) {
-      padding: 0 5px;
-    }
-
-    span {
-      color: rgb(249, 249, 249);
-      font-size: 13px;
-      font-weight: 900;
-      letter-spacing: 0px;
-      line-height: 1.08;
-      padding: 2px 0px;
-      white-space: nowrap;
-      position: relative;
-      @media (max-width: 1000px) {
-        font-size: 10px;
-        font-weight: 900;
-      }
-
-      &:before {
-        background-color: rgb(249, 249, 249);
-        border-radius: 0px 0px 4px 4px;
-        bottom: -6px;
-        content: "";
-        height: 2px;
-        left: 0px;
-        opacity: 0;
-        position: absolute;
-        right: 0px;
-        transform-origin: left center;
-        transform: scaleX(0);
-        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-        visibility: hidden;
-        width: auto;
-      }
-    }
-    &:hover {
-      span:before {
-        transform: scaleX(1);
-        visibility: visible;
-        opacity: 1 !important;
-      }
-    }
-  }
-  @media (max-width: 1000px) {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    margin-left: 0px;
-    width: 90%;
-  }
-  @media (max-width: 500px) {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    margin: 10px;
-    width: 90%;
-  }
-`;
-
 const Square = styled.div`
   display: grid;
   width: 55%;
@@ -198,13 +106,8 @@ const Square = styled.div`
 
   position: absolute;
   right: 0;
-  @media screen and (max-width: 1950px) {
-    width: 45%;
-  }
-  @media screen and (max-width: 1600px) {
-    width: 35%;
-  }
-  @media screen and (max-width: 1300px) {
+
+  @media screen and (max-width: 1200px) {
     width: 100%;
     opacity: 0.4;
   }
@@ -231,7 +134,7 @@ const ImgConatiner = styled.div`
   z-index: 1;
   top: 0;
   right: 0;
-  background: red;
+  background: black;
   opacity: 1;
   display: flex;
   justify-content: center;
@@ -242,20 +145,6 @@ const ImgConatiner = styled.div`
     top: 0;
   } */
 `;
-
-// const ImgConatiner = styled(Image)`
-//   width: 500px;
-//   height: 500px;
-//   position: absolute;
-//   z-index: 1;
-//   background: transparent;
-//   opacity: 0.4;
-//   @media (min-width: 1000px) {
-//     width: 100%;
-//     height: 100%;
-//     top: 0;
-//   }
-// `;
 
 const Gradient = styled.div`
   position: absolute;
@@ -278,20 +167,25 @@ const DescriptionContainer = styled.div`
   justify-content: flex-start;
   align-items: flex-start;
   z-index: 2;
-  width: 90%;
-  height: 60%;
+  width: 45%;
+  height: 580%;
   font-weight: bold;
   text-align: left;
   color: white;
+  position: absolute;
+  left: 0;
+  top: 200px;
 
-  @media screen and (max-width: 1000px) {
+  @media screen and (max-width: 1200px) {
+    top: 0px;
+    width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     text-align: center;
     margin-left: 0px;
-    width: 90%;
   }
 `;
 const SecondImageContainer = styled.div`
@@ -324,19 +218,8 @@ const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const NewsImageWrapper = styled.div`
-  width: 50px;
-  height: 50px;
-  margin: 5px;
-  border-radius: 25px;
-  background-color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
 const HeaderDescription = styled.div`
-  width: 500px;
+  width: 100%;
   height: 100px;
   display: flex;
   flex-direction: column;
@@ -344,44 +227,26 @@ const HeaderDescription = styled.div`
   align-items: flex-start;
   margin-left: 50px;
 
-  /* @media s  @media screen and (max-width: 1000px) {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }creen and (max-width: 1400px) {
-    margin-left: 10px;
-  } */
-  @media screen and (max-width: 1000px) {
+  @media screen and (max-width: 1500px) {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     text-align: center;
     margin-left: 0px;
-    width: 90%;
+    width: 100%;
     margin-bottom: 20px;
   }
   h1 {
-    font-size: 70px;
+    font-size: 60px;
     margin: 0px;
-    /* @media screen and (max-width: 1000px) {
-      font-size: 25px;
-      margin-left: 10px;
-    } */
   }
 `;
 
-const fadeInFromLib1 = keyframes`${fadeInFromLib}`;
 const SecondHeaderDescription = styled.div`
-  width: 600px;
+  width: 100%;
   height: 300px;
-
-  margin-left: 50px;
-  animation-duration: 0.3s;
-  animation-name: ${(props) => (props.active ? fadeInFromLib1 : "")};
-  animation-play-state: running;
+  position: relative;
 
   @media screen and (max-width: 1000px) {
     display: flex;
@@ -393,11 +258,15 @@ const SecondHeaderDescription = styled.div`
     width: 90%;
   }
   h2 {
-    margin: 0px;
+    margin-left: 50px;
     font-weight: 400;
     font-size: 30px;
+    @media screen and (max-width: 1400px) {
+      margin-left: 0px;
+    }
     @media screen and (max-width: 700px) {
       font-size: 15px;
+      margin-left: 0px;
     }
   }
 `;
